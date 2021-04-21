5df0fad9ef3248e141a8b8480cf197802308b8c2
Sarvesh Patil <sarveshspatil111@gmail.com>
2021-04-21 19:48:41 +0530

    Add scripts for system menu

---
+++ .config/i3/scripts/i3exit.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+
+case "$1" in
+    lock)
+        i3lock -i ~/Pictures/walls/ign-0001.png
+        ;;
+    logout)
+        i3-msg exit
+        ;;
+    suspend)
+        systemctl suspend
+        ;;
+    hibernate)
+        systemctl hibernate
+        ;;
+    reboot)
+        systemctl reboot
+        ;;
+    shutdown)
+        systemctl poweroff
+        ;;
+    *)
+        echo "Usage: $0 {lock|logout|suspend|hibernate|reboot|shutdown}"
+        exit 2
+esac
+
+exit 0
